f2fa8231b1c652763aef0132476fedc88bf99ec6
Haltroy <thehaltroy@gmail.com>
2019-08-21 16:30:45 +0300

    Rearranging GitHub Repositories

---
+++ Haltroy.Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using EasyTabs;
+
+namespace Haltroy.Test
+{
+    static class Program
+    {
+        /// <summary>
+        /// Uygulamanın ana girdi noktası.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.Run(new Form1());
+        }
+    }
+}
